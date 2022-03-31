4a1d3c73268ea453719690a9d1f66d94fdfb0df5
camilla <cgreene553@gmail.com>
2022-03-31 10:34:47 +0100

    Edited statement.rb to enable saving of statements and adjusted tests

--- lib/statement.rb
+++ lib/statement.rb
@@ -16,7 +16,6 @@ class Statement
   def deposit(amount)
     @balance += amount
     @transaction_amount = amount
-    @transaction_amount
     @transaction = [date, transaction_amount, balance]
     @bank_statement << transaction
   end
@@ -24,7 +23,6 @@ class Statement
   def withdraw(amount)
     @balance -= amount
     @transaction_amount = amount
-    @transaction_amount
     @transaction = [date, transaction_amount, balance]
     @bank_statement << transaction
   end
@@ -42,8 +40,30 @@ class Statement
 
   def print_format()
     puts print_header
-    bank_statement.reverse.each_slice(1).with_index do |part, ind|
+    @bank_statement.reverse.each_slice(1).with_index do |part, ind|
       puts part.join(" || ") + (ind == 3 ? "" : " ")
     end 
   end
+
+  def save_statement
+    file = File.open("bank_statement.csv", "w")
+    @bank_statement.each do |statement|
+      statement_data = [bank_statement]
+      csv_line = statement_data.join(" || ")
+      file.puts csv_line
+    end
+    file.close
+  end
+
+  def load_statements(filename = "bank_statement.csv")
+    file = File.open(filename, "r") do |file|
+      file.readlines.each do |line|
+        @bank_statement.reverse.each_slice(1).with_index do |part, ind|
+          puts part.join(" || ") + (ind == 3 ? "" : " ")
+        end
+        @transaction = [date, transaction_amount, balance]
+      end
+    end
+  end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -46,7 +46,7 @@ describe Statement do
       statement.deposit(1000.00)
       statement.deposit(2000.00)
       statement.withdraw(500.00)
-      expect(statement.bank_statement).to eq ([["29/03/2022", 1000.0, 1000.0], ["29/03/2022", 2000.0, 3000.0], ["29/03/2022", 500.0, 2500.0]])
+      expect(statement.bank_statement).to eq ([[Time.new.strftime("%d/%m/%Y"), 1000.0, 1000.0], [Time.new.strftime("%d/%m/%Y"), 2000.0, 3000.0], [Time.new.strftime("%d/%m/%Y"), 500.0, 2500.0]])
     end
     describe '#print_header' do
     it "prints the 'header' of a bank statement" do
